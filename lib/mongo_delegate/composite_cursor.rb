270a5fecd2ff8b3562b5e8ba2d75a47c0972ff6c
Mike Harris <mharris717@gmail.com>
2010-01-15 11:54:17 -0500

    sorting works with limit and skip.  not very performant.

--- lib/mongo_delegate/composite_cursor.rb
+++ lib/mongo_delegate/composite_cursor.rb
@@ -1,3 +1,34 @@
+class Mongo::Cursor
+  def foc
+    formatted_order_clause
+  rescue => exp
+    return nil
+  end
+end
+
+class Numeric
+  def sortflip
+    self * -1.0
+  end
+end
+
+class String
+  def num_val
+    return 0 if length == 0
+    self[-1] + 500*self[0..-2].num_val
+  end
+  def sortflip
+    opposite_word
+  end
+  def opposite_word
+    res = " " * length
+    for i in (0...length)
+      res[i] = 255-self[i]
+    end
+    res
+  end
+end
+
 class CompositeCursor
   class FindOps
     attr_accessor :res, :cursor
@@ -7,8 +38,9 @@ class CompositeCursor
     fattr(:found_ids) { res.map { |x| x[1].to_af.map { |x| x['_id'] } }.flatten }
     fattr(:ops) do
       r = {}
-      r[:limit] = (options[:limit] - num_using) if options[:limit]
-      r[:skip] = [(options[:skip] - found_ids.size ),0].max if options[:skip]
+      r[:limit] = (options[:limit] - num_using) if options[:limit] && !options[:sort]
+      r[:skip] = [(options[:skip] - found_ids.size ),0].max if options[:skip] && !options[:sort]
+      r[:sort] = options[:sort] if options[:sort]
       r
     end
     def zero_limit?
@@ -20,8 +52,19 @@ class CompositeCursor
   include FromHash
   include Enumerable
   def to_af; rows; end
+  def sort_proc
+    foc = cursors.first.foc
+    return nil unless foc
+    lambda do |doc|
+      foc.map { |k,v| (v == 1) ? doc[k] : doc[k].sortflip }
+    end
+  end
   fattr(:rows) do
-    cursors.map { |x| x.to_af }.flatten
+    res = cursors.map { |x| x.to_af }.flatten
+    res = res.sort_by(&sort_proc) if sort_proc
+    res = res[0...(options[:limit])] if options[:limit]
+    res = res[(options[:skip])..-1] if options[:skip] && options[:sort]
+    res
   end
   def each(&b)
     rows.each(&b)
